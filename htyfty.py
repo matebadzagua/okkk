8ab2efe2d1da0e63a1ec4cc0cd1e930c0c3460c0
matebadzagua <146456473+matebadzagua@users.noreply.github.com>
2023-10-12 05:15:53 -0700

    Add files via upload

---
+++ htyfty.py
@@ -0,0 +1,10 @@
+username =input ("what is your name, ?")
+userage=input ("what is your age, ?")
+userlastname =input("what is your last name,?")
+momname =input("mom what is your name,?")
+dedname =input("ded what is your name,?")
+petname =input("what pet name,?")
+
+print('username+userage+userlastname+momname+dedname=petname')
+
+print(userage)
